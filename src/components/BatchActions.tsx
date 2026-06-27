f8f0858bd4199c391ba180d1d21dee25a9f71982
mazenfarht <mazenmostafa2022@gmail.com>
2026-06-27 04:35:04 +0300

    edit some error

--- src/components/BatchActions.tsx
+++ src/components/BatchActions.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface BatchActionsProps {
   selectedCount: number;
   onDeleteSelected: () => void;
--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Attendee, SortConfig } from "../types/attendee";
 
 interface TableHeaderProps {
